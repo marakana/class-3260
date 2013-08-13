742afaf7941c2c68373601b313916c6b59f9f638
G. Blake Meike <blake.meike@gmail.com>
2013-08-13 14:23:03 +0000

    Working count

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -3,51 +3,63 @@ package com.marakana.android.yamba;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.content.res.Resources;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class StatusActivity extends Activity {
 
-    private static final long T = System.currentTimeMillis();
+    private int okColor;
+    private int warnColor;
+    private int errColor;
+    private int maxStatusLen;
+    private int warnMax;
+    private int errMax;
+    private TextView viewCount;
+    private EditText viewStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (BuildConfig.DEBUG) {
-            Log.d("###", "created: " + T + ", " + this);
-        }
-
-        String submit = getString(R.string.status_submit);
         super.onCreate(savedInstanceState);
+
+        Resources rez = getResources();
+
+        okColor = rez.getColor(R.color.status_ok);
+        warnColor = rez.getColor(R.color.status_warn);
+        errColor = rez.getColor(R.color.status_err);
+
+        maxStatusLen = rez.getInteger(R.integer.status_max_len);
+        warnMax = rez.getInteger(R.integer.warn_max);
+        errMax = rez.getInteger(R.integer.err_max);
+
         setContentView(R.layout.activity_status);
+
+        viewCount = (TextView) findViewById(R.id.status_count);
+        viewStatus = (EditText) findViewById(R.id.status_status);
+        viewStatus.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) { updateCount(); }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {  }
+        });
     }
 
-    @Override
-    protected void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-    }
+     void updateCount() {
+         int n = maxStatusLen - viewStatus.getText().length();
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
-    }
+         int c;
+         if (n > warnMax) { c = okColor; }
+         else if (n > errMax) { c = warnColor; }
+         else { c = errColor; }
 
-    @Override
-    protected void onResume() {
-        // TODO Auto-generated method stub
-        super.onResume();
+         viewCount.setText(String.valueOf(n));
+         viewCount.setTextColor(c);
     }
-
-    @Override
-    protected void onStart() {
-        // TODO Auto-generated method stub
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        // TODO Auto-generated method stub
-        super.onStop();
-    }
-
 }
